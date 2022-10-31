bc791cf11ab1f73e5a91e377c93acc90daea25f6
Lyncolnlopes <112292071+Lyncolnlopes@users.noreply.github.com>
2022-10-31 09:06:32 -0300

    Delete Front-Enders-main directory

--- Front-Enders-main/js/login.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('#form')
-const botao = document.querySelector('#botao-continuar')
-
-botao.addEventListener('click', () => {
-    alert('Login realizado com sucesso!')
-    form.reset()
-})
